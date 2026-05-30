20786eb4eca8eef698a264d14ca51a157c94e48f
val4nti <val.moralesf@duocuc.cl>
2026-05-30 18:27:10 -0400

    eliminar duplicados

--- src/main/java/com/smartlogix/pedidos/controller/PedidoService.java
+++
@@ -1,4 +0,0 @@
-package com.smartlogix.pedidos.controller;
-
-// Archivo eliminado: era un duplicado erróneo de service/PedidoService.java.
-// El controlador HTTP está en PedidoController.java (mismo paquete).
